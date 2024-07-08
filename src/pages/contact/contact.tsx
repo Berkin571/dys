da25579e2ba55613f0601691472b9787cd20bc42
Berkin Koray <73588639+Berkin95@users.noreply.github.com>
2024-07-08 19:32:22 +0200

    feat: improve contact

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -9,8 +9,12 @@ import {
   Select,
   Textarea,
   SimpleGrid,
+  Heading,
+  Text,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import OfficePicture from "../../assets/office.jpg";
+import "./contact.scss";
 
 interface FormData {
   [key: string]: string;
@@ -79,105 +83,120 @@ export function Contact() {
     console.log(formData, "###");
   };
 
+  const bg = useColorModeValue("gray.50", "gray.800");
+  const color = useColorModeValue("gray.700", "gray.200");
+
   return (
-    <Box
-      maxW={{ base: "full", md: "container.md" }}
-      mx="auto"
-      borderRadius="md"
-    >
-      <img src={OfficePicture} style={{ borderRadius: 5 }} />
+    <>
+      <Box className="image-container">
+        <img src={OfficePicture} alt="Office" className="full-width-image" />
+      </Box>
       <Box
-        as="form"
-        onSubmit={handleSubmit}
         maxW={{ base: "full", md: "container.md" }}
         mx="auto"
         borderRadius="md"
+        bg={bg}
+        color={color}
         p={5}
+        boxShadow="lg"
       >
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
-          <FormControl id="anrede">
-            <FormLabel>Anrede</FormLabel>
-            <Select
-              name="anrede"
-              value={formData.anrede}
-              onChange={handleChange}
+        <Heading as="h1" size="2xl" textAlign="center" mb={6}>
+          Kontaktieren Sie uns
+        </Heading>
+        <Text fontSize="lg" mb={6} textAlign="center">
+          Haben Sie Fragen oder benötigen Sie Unterstützung? Füllen Sie das
+          untenstehende Formular aus, um uns eine Nachricht zu senden. Unser
+          Team wird sich so schnell wie möglich bei Ihnen melden.
+        </Text>
+        <Box as="form" onSubmit={handleSubmit}>
+          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+            <FormControl id="anrede">
+              <FormLabel>Anrede</FormLabel>
+              <Select
+                name="anrede"
+                value={formData.anrede}
+                onChange={handleChange}
+              >
+                <option value="">Anrede</option>
+                <option value="Herr">Herr</option>
+                <option value="Frau">Frau</option>
+                <option value="Dr.">Dr.</option>
+              </Select>
+            </FormControl>
+            <FormControl id="titel">
+              <FormLabel>Titel</FormLabel>
+              <Input
+                placeholder="Titel"
+                type="text"
+                name="titel"
+                value={formData.titel}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl id="firstName">
+              <FormLabel>Vorname</FormLabel>
+              <Input
+                placeholder="Vorname"
+                type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl id="lastName">
+              <FormLabel>Nachname</FormLabel>
+              <Input
+                placeholder="Nachname"
+                type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl
+              id="kanzleiFirma"
+              gridColumn={{ base: "span 1", md: "span 2" }}
             >
-              <option value="">Anrede</option>
-              <option value="Herr">Herr</option>
-              <option value="Frau">Frau</option>
-              <option value="Dr.">Dr.</option>
-            </Select>
-          </FormControl>
-          <FormControl id="titel">
-            <FormLabel>Titel</FormLabel>
-            <Input
-              placeholder="Titel"
-              type="text"
-              name="titel"
-              value={formData.titel}
-              onChange={handleChange}
-            />
-          </FormControl>
-          <FormControl id="firstName">
-            <FormLabel>Vorname</FormLabel>
-            <Input
-              placeholder="Vorname"
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-            />
-          </FormControl>
-          <FormControl id="lastName">
-            <FormLabel>Nachname</FormLabel>
-            <Input
-              placeholder="Nachname"
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-            />
-          </FormControl>
-          <FormControl
-            id="kanzleiFirma"
-            gridColumn={{ base: "span 1", md: "span 2" }}
-          >
-            <FormLabel>Kanzlei / Firma</FormLabel>
-            <Input
-              placeholder="Kanzlei / Firma"
-              type="text"
-              name="kanzleiFirma"
-              value={formData.kanzleiFirma}
-              onChange={handleChange}
-            />
-          </FormControl>
-          <FormControl id="email" gridColumn={{ base: "span 1", md: "span 2" }}>
-            <FormLabel>E-Mail Adresse*</FormLabel>
-            <Input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-              placeholder="E-Mail Adresse*"
-            />
-          </FormControl>
-          <FormControl
-            id="message"
-            gridColumn={{ base: "span 1", md: "span 2" }}
-          >
-            <FormLabel>Nachricht</FormLabel>
-            <Textarea
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-            />
-          </FormControl>
-        </SimpleGrid>
-        <Button type="submit" colorScheme="brand" w="full" mt={4}>
-          Absenden
-        </Button>
+              <FormLabel>Kanzlei / Firma</FormLabel>
+              <Input
+                placeholder="Kanzlei / Firma"
+                type="text"
+                name="kanzleiFirma"
+                value={formData.kanzleiFirma}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl
+              id="email"
+              gridColumn={{ base: "span 1", md: "span 2" }}
+            >
+              <FormLabel>E-Mail Adresse*</FormLabel>
+              <Input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                placeholder="E-Mail Adresse*"
+              />
+            </FormControl>
+            <FormControl
+              id="message"
+              gridColumn={{ base: "span 1", md: "span 2" }}
+            >
+              <FormLabel>Nachricht</FormLabel>
+              <Textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+              />
+            </FormControl>
+          </SimpleGrid>
+          <Button type="submit" colorScheme="brand" w="full" mt={4}>
+            Absenden
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
